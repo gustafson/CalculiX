1eb3342ee32c106c40bc713bf27c8afe4a3ce24b
Peter A. Gustafson <peter.gustafson@wmich.edu>
2013-04-21 23:49:36 -0400

    Exoselect for tensor data appears to work.

--- ccx/src/exoselect.c
+++ ccx/src/exoselect.c
@@ -24,12 +24,13 @@
 
 #define min(a,b) ((a) <= (b) ? (a) : (b))
 #define max(a,b) ((a) >= (b) ? (a) : (b))
+#define rc(r,c) (r+*nkcoords*c)
 
 void exoselect(double *field1,double *field2,int *iset,int *nkcoords,int *inum,
 	       int *istartset,int *iendset,int *ialset,int *ngraph,int *ncomp,
 	       int *ifield,int *icomp,int *nfield,int *iselect,int *exoid,
-	       int *istore, char *vname){
-
+	       int *istore, char *vname, int countvar){
+    
   /* storing scalars, components of vectors and tensors without additional
      transformations */
 
@@ -42,12 +43,11 @@ void exoselect(double *field1,double *field2,int *iset,int *nkcoords,int *inum,
      - icomp[i]: component: 0...,(nfield[0]-1 or nfield[1]-1) */
  
   int i,j,k,l,m,n,nksegment;
-      
+    
   int iw;
 
   float ifl;
 
-  float *nodal_var_vals;
 
   /* When initializing parameter values:
      "g" (or "G")
@@ -62,56 +62,127 @@ void exoselect(double *field1,double *field2,int *iset,int *nkcoords,int *inum,
      For sideset variables.
   */
   int num_nod_vars = 6;
-  nodal_var_vals = (float *) calloc (*nkcoords, sizeof(float));
-  
-  for (j=1; j<=num_nod_vars; j++){ // For each direction
-    if(*iset==0){
-      for(i=0;i<*nkcoords;i++){
+  float *nodal_var_vals;
+  nodal_var_vals = (float *) calloc (*nkcoords * num_nod_vars, sizeof(float));
+  // float nodal_var_vals[*nkcoords][num_nod_vars];
 
-	/* check whether output is requested for solid nodes or
-	   network nodes */
+  // typedef struct {
+  //   int a;
+  //   int b;
+  //   float* data;
+  // } float2d;
+  
+  // float2d arr2d;
+  // arr2d={2,3};
+
+  // float2d.data = malloc(arr2d.a * arr2d.b * sizeof *arr2d.data);
+  
+  // typedef struct {
+  //   int a;
+  //   int b;
+  //   int* data;
+  // } Int2d;
+
+  // Int2d arr2d = { 2, 3 };
+  // arr2d.data = malloc(arr2d.a * arr2d.b * sizeof *arr2d.data);
+
+  if(*iset==0){
+    for(i=0;i<*nkcoords;i++){
       
-	if(*iselect==1){
-	  if(inum[i]<=0) continue;
-	}else if(*iselect==-1){
-	  if(inum[i]>=0) continue;
-	}else{
-	  if(inum[i]==0) continue;
-	}
+      /* check whether output is requested for solid nodes or
+	 network nodes */
       
-	/* storing the entities */
+      if(*iselect==1){
+	if(inum[i]<=0) continue;
+      }else if(*iselect==-1){
+	if(inum[i]>=0) continue;
+      }else{
+	if(inum[i]==0) continue;
+      }
       
-	for(n=1;n<=(int)((*ncomp+5)/6);n++){
-	  if(n==1){
-	    printf("%3s%10d","\nm1",i+1);
-	    for(j=0;j<min(6,*ncomp);j++){
-	      if(ifield[j]==1){
-		printf("%12.5E",field1[i*nfield[0]+icomp[j]]);
-	      }else{
-		printf("%12.5E",field2[i*nfield[1]+icomp[j]]);
-	      }
+      /* storing the entities */
+      
+      for(n=1;n<=(int)((*ncomp+5)/6);n++){
+	if(n==1){
+	  // printf("%3s%10d","\nm1",i+1);
+	  for(j=0;j<min(6,*ncomp);j++){
+	    if(ifield[j]==1){
+	      // printf("%12.5E",field1[i*nfield[0]+icomp[j]]);
+	      nodal_var_vals[rc(i,j)]=field1[i*nfield[0]+icomp[j]];
+	    }else{
+	      // printf("%12.5E",field2[i*nfield[1]+icomp[j]]);
+	      nodal_var_vals[rc(i,j)]=field2[i*nfield[1]+icomp[j]];
 	    }
-	  }else{
-	    for(j=(n-1)*6;j<min(n*6,*ncomp);j++){
-	      if(ifield[j]==1){
-		printf("%12.5E",field1[i*nfield[0]+icomp[j]]);
-	      }else{
-		printf("%12.5E",field2[i*nfield[1]+icomp[j]]);
-	      }
+	  }
+	}else{
+	  for(j=(n-1)*6;j<min(n*6,*ncomp);j++){
+	    if(ifield[j]==1){
+	      // printf("%12.5E",field1[i*nfield[0]+icomp[j]]);
+	      nodal_var_vals[rc(i,j)]=field1[i*nfield[0]+icomp[j]];
+	    }else{
+	      // printf("%12.5E",field2[i*nfield[1]+icomp[j]]);
+	      nodal_var_vals[rc(i,j)]=field2[i*nfield[1]+icomp[j]];
 	    }
 	  }
 	}
       }
-    }else{
-      nksegment=(*nkcoords)/(*ngraph);
-      for(k=istartset[*iset-1]-1;k<iendset[*iset-1];k++){
-	if(ialset[k]>0){
-	  for(l=0;l<*ngraph;l++){
-	    i=ialset[k]+l*nksegment-1;
+    }
+  }else{
+    nksegment=(*nkcoords)/(*ngraph);
+    for(k=istartset[*iset-1]-1;k<iendset[*iset-1];k++){
+      if(ialset[k]>0){
+	for(l=0;l<*ngraph;l++){
+	  i=ialset[k]+l*nksegment-1;
 
+	  /* check whether output is requested for solid nodes or
+	     network nodes */
+
+	  if(*iselect==1){
+	    if(inum[i]<=0) continue;
+	  }else if(*iselect==-1){
+	    if(inum[i]>=0) continue;
+	  }else{
+	    if(inum[i]==0) continue;
+	  }
+	  
+	  /* storing the entities */
+	  for(n=1;n<=(int)((*ncomp+5)/6);n++){
+	    if(n==1){
+	      // printf("%3s%10d","\nm1",i+1);
+	      for(j=0;j<min(6,*ncomp);j++){
+		if(ifield[j]==1){
+		  // printf("%12.5E",field1[i*nfield[0]+icomp[j]]);
+		  nodal_var_vals[rc(i,j)]=field1[i*nfield[0]+icomp[j]];
+		}else{
+		  // printf("%12.5E",field2[i*nfield[1]+icomp[j]]);
+		  nodal_var_vals[rc(i,j)]=field2[i*nfield[1]+icomp[j]];
+		}
+	      }
+	    }else{
+	      for(j=(n-1)*6;j<min(n*6,*ncomp);j++){
+		if(ifield[j]==1){
+		  // printf("%12.5E",field1[i*nfield[0]+j]);
+		  nodal_var_vals[rc(i,j)]=field1[i*nfield[0]+j];
+		}else{
+		  // printf("%12.5E",field2[i*nfield[1]+j]);
+		  nodal_var_vals[rc(i,j)]=field2[i*nfield[1]+j];
+		}
+	      }
+	    }
+	  }
+	  
+	}
+      }else{
+	l=ialset[k-2];
+	do{
+	  l-=ialset[k];
+	  if(l>=ialset[k-1]) break;
+	  for(m=0;m<*ngraph;m++){
+	    i=l+m*nksegment-1;
+	    
 	    /* check whether output is requested for solid nodes or
 	       network nodes */
-
+	    
 	    if(*iselect==1){
 	      if(inum[i]<=0) continue;
 	    }else if(*iselect==-1){
@@ -119,83 +190,52 @@ void exoselect(double *field1,double *field2,int *iset,int *nkcoords,int *inum,
 	    }else{
 	      if(inum[i]==0) continue;
 	    }
-	  
+	    
 	    /* storing the entities */
+	    
 	    for(n=1;n<=(int)((*ncomp+5)/6);n++){
 	      if(n==1){
-		printf("%3s%10d","\nm1",i+1);
+		// printf("%3s%10d","\nm1",i+1);
 		for(j=0;j<min(6,*ncomp);j++){
 		  if(ifield[j]==1){
-		    printf("%12.5E",field1[i*nfield[0]+icomp[j]]);
+		    // printf("%12.5E",field1[i*nfield[0]+icomp[j]]);
+		    nodal_var_vals[rc(i,j)]=field1[i*nfield[0]+icomp[j]];
 		  }else{
-		    printf("%12.5E",field2[i*nfield[1]+icomp[j]]);
+		    // printf("%12.5E",field2[i*nfield[1]+icomp[j]]);
+		    nodal_var_vals[rc(i,j)]=field2[i*nfield[1]+icomp[j]];
 		  }
 		}
 	      }else{
 		for(j=(n-1)*6;j<min(n*6,*ncomp);j++){
 		  if(ifield[j]==1){
-		    printf("%12.5E",field1[i*nfield[0]+j]);
+		    // printf("%12.5E",field1[i*nfield[0]+j]);
+		    nodal_var_vals[rc(i,j)]=field1[i*nfield[0]+j];
 		  }else{
-		    printf("%12.5E",field2[i*nfield[1]+j]);
+		    // printf("%12.5E",field2[i*nfield[1]+j]);
+		    nodal_var_vals[rc(i,j)]=field2[i*nfield[1]+j];
 		  }
 		}
 	      }
 	    }
-	  
 	  }
-	}else{
-	  l=ialset[k-2];
-	  do{
-	    l-=ialset[k];
-	    if(l>=ialset[k-1]) break;
-	    for(m=0;m<*ngraph;m++){
-	      i=l+m*nksegment-1;
-	    
-	      /* check whether output is requested for solid nodes or
-		 network nodes */
-	    
-	      if(*iselect==1){
-		if(inum[i]<=0) continue;
-	      }else if(*iselect==-1){
-		if(inum[i]>=0) continue;
-	      }else{
-		if(inum[i]==0) continue;
-	      }
-	    
-	      /* storing the entities */
-	    
-	      for(n=1;n<=(int)((*ncomp+5)/6);n++){
-		if(n==1){
-		  printf("%3s%10d","\nm1",i+1);
-		  for(j=0;j<min(6,*ncomp);j++){
-		    if(ifield[j]==1){
-		      printf("%12.5E",field1[i*nfield[0]+icomp[j]]);
-		    }else{
-		      printf("%12.5E",field2[i*nfield[1]+icomp[j]]);
-		    }
-		  }
-		}else{
-		  for(j=(n-1)*6;j<min(n*6,*ncomp);j++){
-		    if(ifield[j]==1){
-		      printf("%12.5E",field1[i*nfield[0]+j]);
-		    }else{
-		      printf("%12.5E",field2[i*nfield[1]+j]);
-		    }
-		  }
-		}
-	      }
-	    }
-	  }while(1);
-	}
+	}while(1);
       }
     }
+  }
     
-    printf ("\n");
-    int errr = ex_put_nodal_var (exoid, istore, j, *nkcoords, nodal_var_vals);
-    if (errr) printf ("ERROR storing vector data into exo file.\n");
-  }  
+  
+  float *nodal_var_vals_out;
+  nodal_var_vals_out = (float *) calloc (*nkcoords, sizeof(float));
+  for (j=0; j<num_nod_vars; j++){
+    for (i=0; i<*nkcoords; i++){
+      nodal_var_vals_out[i]=nodal_var_vals[rc(i,j)];
+    }
+    int errr = ex_put_nodal_var (exoid, istore, j+1+countvar, *nkcoords, nodal_var_vals_out);
+    if (errr) printf ("ERROR storing data into exo file for dim %i record %i.\n", j, countvar+j);
+  }
 
   free(nodal_var_vals);
+  free(nodal_var_vals_out);
   return;
 
 }
