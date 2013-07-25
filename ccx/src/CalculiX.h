f3af3a48377584eb6fb9070f33252314b9f2faff
Peter A. Gustafson <peter.gustafson@wmich.edu>
2013-07-24 20:28:22 -0400

    Opens and write an frd file with exo extension.

--- ccx/src/CalculiX.h
+++ ccx/src/CalculiX.h
@@ -1106,6 +1106,21 @@ void frd(double *co,int *nk,int *kon,int *ipkon,char *lakon,int *ne0,
 	 double *eenmax,double *fnr,double *fni,double *emn,
 	 double *thicke,char *jobnamec, char *output,double *qfx);
 
+void exo(double *co,int *nk,int *kon,int *ipkon,char *lakon,int *ne0,
+	 double *v,double *stn,int *inum,int *nmethod,int *kode,
+	 char *filab,double *een,double *t1,double *fn,double *time,
+	 double *epn,int *ielmat,char *matname,double *enern,
+	 double *xstaten,int *nstate_,int *istep,int *iinc,
+	 int *ithermal,double *qfn,int *mode,int *noddiam,
+	 double *trab,int *inotr,int *ntrans,double *orab,
+	 int *ielorien,int *norien,char *description,int *ipneigh,
+	 int *neigh,int *mi,double *stx,double *vr,double *vi,
+	 double *stnr,double *stni,double *vmax,double *stnmax,
+	 int *ngraph,double *veold,double *ener,int *ne,double *cs,
+	 char *set,int *nset,int *istartset,int *iendset,int *ialset,
+	 double *eenmax,double *fnr,double *fni,double *emn,
+	 double *thicke,char *jobnamec, char *output);
+
 void frdcyc(double *co,int *nk,int *kon,int *ipkon,char *lakon,int *ne,double *v,
 	    double *stn,int *inum,int *nmethod,int *kode,char *filab,
 	    double *een,double *t1,double *fn,double *time,double *epn,
