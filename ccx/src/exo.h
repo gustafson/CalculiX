c94291679fcd58bb77425cd39d8bce54e4cd60c4
Peter A. Gustafson <peter.gustafson@wmich.edu>
2014-03-03 09:16:12 -0500

    Updating copyright.

--- ccx/src/exo.h
+++ ccx/src/exo.h
@@ -1,3 +1,22 @@
+/*     Calculix - A 3-dimensional finite element program                 */
+/*              Copyright (C) 1998-2014 Guido Dhondt                     */
+/*     This subroutine                                                   */
+/*              Copyright (C) 2013-2014 Peter A. Gustafson               */
+/*                                                                       */
+/*     This program is free software; you can redistribute it and/or     */
+/*     modify it under the terms of the GNU General Public License as    */
+/*     published by the Free Software Foundation(version 2);    */
+/*                                                                       */
+
+/*     This program is distributed in the hope that it will be useful,   */
+/*     but WITHOUT ANY WARRANTY; without even the implied warranty of    */ 
+/*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the      */
+/*     GNU General Public License for more details.                      */
+
+/*     You should have received a copy of the GNU General Public License */
+/*     along with this program; if not, write to the Free Software       */
+/*     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.         */
+
 void exo(double *co,int *nk,int *kon,int *ipkon,char *lakon,int *ne0,
 	 double *v,double *stn,int *inum,int *nmethod,int *kode,
 	 char *filab,double *een,double *t1,double *fn,double *time,
