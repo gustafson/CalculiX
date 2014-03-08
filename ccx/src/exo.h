892cbf227effca266c6c49e1f582407515421ca5
Peter A. Gustafson <peter.gustafson@wmich.edu>
2014-03-08 00:57:17 -0500

    Fixing bug where undefined nodes in a set would cause crash.

--- ccx/src/exo.h
+++ ccx/src/exo.h
@@ -34,19 +34,14 @@ void exo(double *co,int *nk,int *kon,int *ipkon,char *lakon,int *ne0,
 
 void exosetfind(char *set, int *nset, int *ialset, int *istartset, int *iendset, 
 		int *num_ns, int *num_ss, int *num_es, int *num_fs, int *node_map_inv,
-		int exoid, 
-		int store);
+		int exoid, int store, int *nk);
 
-void exoset_warn(int val);
-
-int exoset_check(int n, int *node_map_inv);
+int exoset_check(int n, int *node_map_inv, int *nk);
 
 void exovector(double *v,int *iset,int *ntrans,char * filabl,int *nkcoords,
                int *inum,int *inotr,double *trab,double *co,
                int *istartset,int *iendset,int *ialset,int *mi,int *ngraph,
                int exoid, int time_step, int countvar, int nout);
-
-int exoset_check(int n, int *node_map_inv);
 
 void exoselect(double *field1,double *field2,int *iset,int *nkcoords,int *inum,
 	       int *istartset,int *iendset,int *ialset,int *ngraph,int *ncomp,
