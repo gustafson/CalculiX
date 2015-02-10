1cf59ecceab8ca06037fa844ddfe9ba2b4ac92cc
Peter A. Gustafson <peter.gustafson@wmich.edu>
2015-02-09 23:51:32 -0500

    Perhaps some progress... debugging cholmod.

--- ccx/src/suitesparse.c
+++ ccx/src/suitesparse.c
@@ -38,16 +38,21 @@ int suitesparsecholmod (double *ad, double *au, double *adb, double *aub, double
   cholmod_dense *X, *B;
   double one[2] = {1,0};
 
-  int symmetric = 1;
+  int issymmetric = 1; 
+  printf( "long long is %i bits\n", sizeof(long long));
+  printf( "long is %i bits\n", sizeof(long));
+  printf( "int is %i bits\n", sizeof(int));
+  printf( "ITG is %i bits\n", sizeof(ITG));
+  printf( "SuiteSparse_long is %i bits\n", sizeof(SuiteSparse_long));
 
   // start CHOLMOD
   ccc = &Common;
 #ifdef LONGLONG
+  printf ("Using long version of cholmod.\n");
   cholmod_l_start (ccc);
 #else
   cholmod_start (ccc);
 #endif  
-  
   // load A 
   // Square symmetric upper triangular. 
   cholmod_triplet *AUT;
@@ -56,8 +61,8 @@ int suitesparsecholmod (double *ad, double *au, double *adb, double *aub, double
   AUT = cholmod_l_allocate_triplet(*neq, *neq, *nzs, issymmetric, CHOLMOD_REAL, ccc);
   ADT = cholmod_l_allocate_triplet(*neq, *neq, *neq, issymmetric, CHOLMOD_REAL, ccc);
 #else
-  AUT = cholmod_allocate_triplet(*neq, *neq, *nzs, 1, CHOLMOD_REAL, ccc);
-  ADT = cholmod_allocate_triplet(*neq, *neq, *neq, 1, CHOLMOD_REAL, ccc);
+  AUT = cholmod_allocate_triplet(*neq, *neq, *nzs, issymmetric, CHOLMOD_REAL, ccc);
+  ADT = cholmod_allocate_triplet(*neq, *neq, *neq, issymmetric, CHOLMOD_REAL, ccc);
 #endif
   
   ITG i,j,l,m;
@@ -112,8 +117,8 @@ int suitesparsecholmod (double *ad, double *au, double *adb, double *aub, double
   }
   
   int num_cpus=1;
-#if USE_MT
   char *env;
+#if USE_MT
   env=getenv("OMP_NUM_THREADS");
   if(env) {
     num_cpus=atoi(env);}
@@ -122,9 +127,29 @@ int suitesparsecholmod (double *ad, double *au, double *adb, double *aub, double
   }
 #endif
 
+  //
+#if GPU_BLAS
+  // GPU can be forced but only works with longlong
+  // SHOULD BE AUTOMATIC // env=getenv("CHOLMOD_GPU_MEM_BYTES");
+  // SHOULD BE AUTOMATIC // if(env) {
+  // SHOULD BE AUTOMATIC //   num_cpus=atoi(env);
+  // SHOULD BE AUTOMATIC //   ccc->maxGpuMemBytes=num_cpus;
+  // SHOULD BE AUTOMATIC // }
+
+  // SHOULD BE AUTOMATIC // env=getenv("CHOLMOD_USE_GPU");
+  // SHOULD BE AUTOMATIC // if(env) {
+  // SHOULD BE AUTOMATIC //   num_cpus=atoi(env);}
+  // SHOULD BE AUTOMATIC // else{
+  // SHOULD BE AUTOMATIC //   num_cpus=-1;
+  // SHOULD BE AUTOMATIC // }
+  // SHOULD BE AUTOMATIC // ccc->useGPU = num_cpus;
+  
+  // CONSIDER ALSO CHOLMOD_GPU_MEM_FRACTION
+#endif
+
   // Blas itself should obey OMP_NUM_THREADS.  Set SPQR threads
-  // Set the number of threads
-  // ccc->SPQR_nthreads = num_cpus;
+  // Set the number of TBB threads
+  ccc->SPQR_nthreads = num_cpus;
 
   // Force the use of supernodal if you want to force CUDA
   // ccc->supernodal = CHOLMOD_SUPERNODAL;
@@ -136,10 +161,11 @@ int suitesparsecholmod (double *ad, double *au, double *adb, double *aub, double
   // ccc->nmethods = 1;
   // ccc->method[0].ordering = CHOLMOD_NATURAL;
   // ccc->method[0].ordering = CHOLMOD_AMD;
+  // ccc->method[0].ordering = CHOLMOD_METIS;
   // ccc->postorder = 0;
 
   // Set to 2, 4, or 8 depending on available memory.  (8 requires more)
-  // ccc->maxrank=8;
+  ccc->maxrank=8;
 
   cholmod_factor *L ;
 #ifdef LONGLONG
@@ -149,6 +175,7 @@ int suitesparsecholmod (double *ad, double *au, double *adb, double *aub, double
   L = cholmod_analyze (A, ccc) ;
   cholmod_factorize (A, L, ccc) ;
 #endif
+  // printf("  The estimated reciprocal condition number is %g\n", cholmod_rcond(L, ccc));
 
   if (ccc->status==CHOLMOD_NOT_POSDEF) {
     printf("  Stiffnessmatrix is not positive definite in column %" ITGFORMAT ".\n\n", L->minor);
@@ -205,39 +232,47 @@ int suitesparseqr (double *ad, double *au, double *adb, double *aub, double *sig
 {
   
   cholmod_common Common, *ccc ;
-  cholmod_sparse *A ;
+  cholmod_sparse *A;
   cholmod_dense *X, *B;
   double one[2] = {1,0};
 
-  int symmetric = 0;
+  // spqr requires an unsymmetric matrix
+  int issymmetric = 0; 
+
+  printf( "long long is %i bits\n", sizeof(long long));
+  printf( "long is %i bits\n", sizeof(long));
+  printf( "int is %i bits\n", sizeof(int));
+  printf( "ITG is %i bits\n", sizeof(ITG));
+  printf( "SuiteSparse_long is %i bits\n", sizeof(SuiteSparse_long));
 
   // start CHOLMOD
   ccc = &Common;
   cholmod_l_start (ccc);
 
   // load A 
-  // Square symmetric upper triangular. 
+  // Square issymmetric upper triangular. 
   cholmod_triplet *AUT;
   cholmod_triplet *ADT;
-  AUT = cholmod_l_allocate_triplet(*neq, *neq, *nzs, 0, CHOLMOD_REAL, ccc);
-  ADT = cholmod_l_allocate_triplet(*neq, *neq, *neq, 0, CHOLMOD_REAL, ccc);
-  
-  ITG i,j,l,m;
+  AUT = cholmod_l_allocate_triplet(*neq, *neq, *nzs, issymmetric, CHOLMOD_REAL, ccc);
+  ADT = cholmod_l_allocate_triplet(*neq, *neq, *neq, issymmetric, CHOLMOD_REAL, ccc);
+
+  SuiteSparse_long i,j,l,m;
   l=0; // row index
   m=0; // column tracker index
   for (i = 0; i < *neq; i++){
     for (j = 0; j < icol[i]; j++){
-      ((ITG*)AUT->i)[AUT->nnz] = l; 
-      ((ITG*)AUT->j)[AUT->nnz] = (irow[m]-1); 
+      // printf ("%" ITGFORMAT " %" ITGFORMAT " %g\n", l, (irow[m]-1), au[m]);
+      ((SuiteSparse_long*)AUT->i)[AUT->nnz] = l; 
+      ((SuiteSparse_long*)AUT->j)[AUT->nnz] = (irow[m]-1); 
       ((double*)AUT->x)[AUT->nnz] = au[m++];
-      (AUT->nnz)++;
+    (AUT->nnz)++;
     } l++;
   }
-  
+
   // Now add the Diagonal matrix
   for (i = 0; i < *neq; i++){
-    ((ITG*)ADT->i)[ADT->nnz] = i; 
-    ((ITG*)ADT->j)[ADT->nnz] = i;
+    ((SuiteSparse_long*)ADT->i)[ADT->nnz] = i; 
+    ((SuiteSparse_long*)ADT->j)[ADT->nnz] = i;
     ((double*)ADT->x)[ADT->nnz] = ad[i];
     (ADT->nnz)++;
   }
@@ -247,9 +282,11 @@ int suitesparseqr (double *ad, double *au, double *adb, double *aub, double *sig
   AU = cholmod_l_triplet_to_sparse (AUT, 0, ccc) ;
   cholmod_l_free_triplet (&AUT, ccc) ;
   cholmod_l_free_triplet (&ADT, ccc) ;
-  
-  A = cholmod_l_add (AU, A, one, one, 1, 1, ccc);
-  A = cholmod_l_add (A, cholmod_l_transpose (AU, 2, ccc), one, one, 1, 1, ccc);
+
+  int inumerical=1;
+  int isort=0;
+  A = cholmod_l_add (AU, A, one, one, inumerical, isort, ccc);
+  A = cholmod_l_add (A, cholmod_l_transpose (AU, 2, ccc), one, one, inumerical, isort, ccc);
   cholmod_l_free_sparse (&AU, ccc) ;
   
   // B = ones (size (A,1),1)
@@ -281,9 +318,10 @@ int suitesparseqr (double *ad, double *au, double *adb, double *aub, double *sig
   // Set to 2, 4, or 8 depending on available memory.  (8 requires more)
   // ccc->maxrank=8;
 
+  cholmod_l_print_common("Common", ccc);
   printf("  Factoring with SuiteSparseQR using %i threads\n", num_cpus);
   // X = SuiteSparseQR <double> (A, B, ccc); // Could be used for c++ version
-  printf ("SPQR Bonks here but has been so slow that I'm not fixing it for now.:\n");
+  printf ("SPQR Bonks here with long integers... but it has been so slow that I'm not fixing it for now.\n");
   X = SuiteSparseQR_C_backslash_default (A, B, ccc);
   printf ("    Number of equations %" ITGFORMAT ": rank of the matrix %ld\n\n", *neq, ccc->SPQR_istat[4]) ;
   cholmod_l_print_common("Common", ccc);
--- ccx/src/suitesparse.h
+++ ccx/src/suitesparse.h
@@ -2,6 +2,43 @@
 extern "C"
 #endif
 
+#ifdef LONGLONG
+
+// #ifndef SuiteSparse_long
+// 
+// #ifdef _WIN64
+// #define SuiteSparse_long __int64
+// #define SuiteSparse_long_max _I64_MAX
+// #define SuiteSparse_long_idd "I64d"
+// #else
+#define SuiteSparse_long long long
+#define SuiteSparse_long_max LONG_LONG_MAX
+#define SuiteSparse_long_idd "lld"
+// #endif
+// 
+// #define SuiteSparse_long_id "%" SuiteSparse_long_idd
+// #endif
+// 
+// #else
+// 
+// #ifndef SuiteSparse_long
+// 
+// #ifdef _WIN64
+// #define SuiteSparse_long __int64
+// #define SuiteSparse_long_max _I64_MAX
+// #define SuiteSparse_long_idd "I64d"
+// 
+// #define SuiteSparse_long long
+// #define SuiteSparse_long_max LONG_MAX
+// #define SuiteSparse_long_idd "ld"
+//
+//#endif
+#define SuiteSparse_long_id "%" SuiteSparse_long_idd
+// #endif
+
+#endif
+
+
 int suitesparsecholmod(double *ad, double *au, double *adb, double *aub, double *sigma,
 		       double *b, ITG *icol, ITG *irow, ITG *neq, ITG *nzs);
 
